07416fd2b4c84500345f06b25b86879c0d8a56b3
ESmotor <e-skidan@mail.ru>
2023-04-24 08:05:32 +0300

    First Commit

---
+++ src/Calculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+//dsfsdfsdfsdfsdfbchdhdfhdfg
+///jjfgjfghfghf
+
+
+
+    public class Calculator {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введите первое число: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Введите второе число: ");
+            double num2 = scanner.nextDouble();
+
+            System.out.print("Введите операцию (+, -, *, /): ");
+            char operator = scanner.next().charAt(0);
+
+            double result;
+
+            if (operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                result = num1 / num2;
+            } else {
+                System.out.println("Неверная операция!");
+                return;
+            }
+
+            System.out.println("Результат: " + result);
+        }
+    }
+
